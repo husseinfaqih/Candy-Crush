0fb2d8ff155ca615d0daa9a1428f3ca5d43e1cc0
Aleksei Kudriavtcev <ackudryavcev@gmail.com>
2023-04-19 15:43:01 +0200

    added components to promotion section added popup to promotion

---
+++ client/src/components/promotion/PromotionImage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PromotionImage() {
+  return (
+    <img
+      className="promotion-image"
+      src="https://lookw.ru/19/1905/1638926038-1009-www.lookw.ru.jpg"
+      alt="candy"
+    />
+  );
+}
+
+export default PromotionImage;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import TEST_ID from "./Home.testid";
+import Promotion from "../../components/promotion/Promotion";
 
 const Home = () => {
   return (
     <div data-testid={TEST_ID.container}>
+      <Promotion />
       <h1>This is the homepage</h1>
       <p>Good luck with the project!</p>
     </div>
