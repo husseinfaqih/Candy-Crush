2a08b22d1aa2ce61271c762baf0b0e73d3c1ad3d
Hussein <husseinfaqih@gmail.com>
2023-04-20 15:32:42 +0200

    added an allproduct path and page and ready to push and merge 2

--- client/src/pages/AllProduct/AllProduct.jsx
+++ client/src/pages/AllProduct/AllProduct.jsx
@@ -1,14 +1,13 @@
-import React from "react"
-import ProductDisplay from "../../components/ProductDisplay/ProductDisplay"
-
+import React from "react";
+import ProductDisplay from "../../components/ProductDisplay/ProductDisplay";
 
 const AllProduct = () => {
   return (
     <div>
-    <h1>This is the All Product Page</h1>
-    <ProductDisplay />
-  </div>
-  )
-}
+      <h1>This is the All Product Page</h1>
+      <ProductDisplay />
+    </div>
+  );
+};
 
-export default AllProduct
+export default AllProduct;
