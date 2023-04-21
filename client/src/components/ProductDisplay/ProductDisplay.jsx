8e28acf593d612d0a4c4c51561e2bb391be884e4
Hussein <husseinfaqih@gmail.com>
2023-04-21 18:09:42 +0200

    seeding product from database

--- client/src/components/ProductDisplay/ProductDisplay.jsx
+++ client/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,26 +1,53 @@
 import React from "react";
-import { products } from "../../ProductDataDummy";
+// import { products } from "../../ProductDataDummy";
 import ProductOverview from "./ProductOverview";
 import Rating from "./Rating";
 import Favorite from "./Favorite";
 import Basket from "./Basket";
+import useFetch from "../../hooks/useFetch";
+import { useState, useEffect } from "react";
 
 const ProductDisplay = () => {
-  return (
-    <div className="product-display-grid">
-      {products.map((product) => {
-        return (
-          <div className="product-display-component" key={product.id}>
-            <ProductOverview product={product} />
-
-            <Rating productRating={product.rating} />
-            <Favorite />
-            <Basket />
-          </div>
-        );
-      })}
-    </div>
+  const [products, setProducts] = useState(null);
+  const { isLoading, error, performFetch, cancelFetch } = useFetch(
+    "/product",
+    (response) => {
+      setProducts(response.result);
+    }
   );
+
+  useEffect(() => {
+    performFetch();
+
+    return cancelFetch;
+  }, []);
+
+  let content = null;
+
+  if (isLoading) {
+    content = <div>loading...</div>;
+  } else if (error != null) {
+    content = <div>Error: {error.toString()}</div>;
+  } else {
+    content = (
+      <div className="product-display-grid">
+        {products &&
+          products.map((product) => {
+            return (
+              <div className="product-display-component" key={product.id}>
+                <ProductOverview product={product} />
+
+                <Rating productRating={product.rate} />
+                <Favorite />
+                <Basket />
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+
+  return <div>{content}</div>;
 };
 
 export default ProductDisplay;
