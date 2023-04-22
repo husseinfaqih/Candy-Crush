1c9bbede06a7d8d8020bc675e78690cb0d51a92d
Ahmad <achmed.alsabbgh@gmail.com>
2023-04-22 15:51:01 +0200

    added sort and filter to allProduct page

--- client/src/pages/AllProduct/AllProduct.jsx
+++ client/src/pages/AllProduct/AllProduct.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProductDisplay from "../../components/ProductDisplay/ProductDisplay";
+import SortAndFilter from "../../components/Sort And Filter/SortAndFilter";
 
 const AllProduct = () => {
   return (
     <div>
       <h1>This is the All Product Page</h1>
+      <SortAndFilter />
       <ProductDisplay />
     </div>
   );
