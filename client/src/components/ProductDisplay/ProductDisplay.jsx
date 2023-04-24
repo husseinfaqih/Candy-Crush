cf68b009c88b65040dae4f34732566fc7fde1a23
Hussein <husseinfaqih@gmail.com>
2023-04-24 14:48:52 +0200

    infinite scrolling is implemented

--- client/src/components/ProductDisplay/ProductDisplay.jsx
+++ client/src/components/ProductDisplay/ProductDisplay.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { products } from "../../ProductDataDummy";
 import ProductOverview from "./ProductOverview";
 import Rating from "./Rating";
 import Favorite from "./Favorite";
@@ -8,42 +7,67 @@ import useFetch from "../../hooks/useFetch";
 import { useState, useEffect } from "react";
 
 const ProductDisplay = () => {
-  const [products, setProducts] = useState(null);
-  const { isLoading, error, performFetch, cancelFetch } = useFetch(
-    "/product?page=0",
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [loadingProducts, setLoadingProducts] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
+
+  const { isLoading, error, performFetch } = useFetch(
+    `/product?page=${page}`,
     (response) => {
-      setProducts(response.result);
+      if (response.result.length === 0 || error != null) {
+        setHasMore(false);
+      } else {
+        setProducts((prevProducts) => [...prevProducts, ...response.result]);
+      }
+      setLoadingProducts(false);
     }
   );
 
   useEffect(() => {
     performFetch();
+  }, [page]);
 
-    return cancelFetch;
-  }, []);
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const windowInnerHeight = window.innerHeight;
+
+    if (windowInnerHeight + scrollTop + 1 >= scrollHeight && hasMore) {
+      setPage((prev) => prev + 1);
+      setLoadingProducts(true);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [hasMore]);
 
   let content = null;
 
-  if (isLoading) {
+  if (isLoading && products.length === 0) {
     content = <div>loading...</div>;
   } else if (error != null) {
     content = <div>Error: {error.toString()}</div>;
   } else {
     content = (
-      <div className="product-display-grid">
-        {products &&
-          products.map((product) => {
+      <>
+        <div className="product-display-grid">
+          {products.map((product) => {
             return (
               <div className="product-display-component" key={product.id}>
                 <ProductOverview product={product} />
-
                 <Rating productRating={product.rate} />
                 <Favorite />
                 <Basket />
               </div>
             );
           })}
-      </div>
+        </div>
+        {loadingProducts && <h1>Loading Products</h1>}
+        {!hasMore && <h1>No more products to fetch</h1>}
+      </>
     );
   }
 
