f5a8f7dcaf2b7ef1b3269c1d651a15d32051d34f
Ahmad <achmed.alsabbgh@gmail.com>
2023-04-25 23:20:24 +0200

    merge complete

--- client/src/components/category/Category.jsx
+++ client/src/components/category/Category.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./categories.css";
 
-export default function Category({ imageUrl, headerText, descText }) {
+export default function Category({ image, categoryName, description }) {
   return (
     <div className="categories-container">
-      <img src={imageUrl} alt="" className="categories-image" />
+      <img src={image} alt="" className="categories-image" />
       <div className="categories-layer">
         <div className="categories-text">
-          <h3 className="categories-header-text">{headerText}</h3>
-          <span className="categories-description">{descText}</span>
+          <h3 className="categories-header-text">{categoryName}</h3>
+          <span className="categories-description">{description}</span>
         </div>
       </div>
       <button className="categories-button">SHOP NOW</button>
