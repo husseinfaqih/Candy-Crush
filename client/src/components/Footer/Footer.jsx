79c765e24f3ace5b0b29faf0ce755bc0722fc673
Ahmad <achmed.alsabbgh@gmail.com>
2023-05-01 22:56:03 +0200

    footer impovments

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,57 +5,38 @@ import Socialmedia from "../Socialmedia";
 function Footer() {
   return (
     <footer>
-      <div className="footer-container">
-        <div className="footer-information">
-          <h4>Catagories</h4>
-          <div className="footer-catagory">
-            <div className="footer-col">
-              <ul>
-                <li>Cookies</li>
-                <li>Candy Corn</li>
-                <li>Lollipop</li>
-                <li>Hard Candy</li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <ul>
-                <li>Vegetarian</li>
-                <li>Chocolate</li>
-                <li>Candy Canes</li>
-                <li>Candy Coated</li>
-              </ul>
-            </div>
-            <div className="footer-col">
-              <ul>
-                <li>Cookies</li>
-                <li>Candy Corn</li>
-                <li>Lollipop</li>
-                <li>Hard Candy</li>
-              </ul>
-            </div>
-          </div>
-          <div className="footer-contact">
+      <div className="footer-information">
+        <h4>Categories</h4>
+        <div className="footer-category">
+          <div className="footer-col">
             <ul>
-              <li>Phone : 06-123-321-89</li>
-              <li>Email : info@sugarrush.com</li>
-              <li>Adress: Sugarrush 1 , 8937 SR Netherland</li>
+              <li>Cookies</li>
+              <li>Candy Corn</li>
+              <li>Lollipop</li>
+              <li>Hard Candy</li>
+              <li>Vegetarian</li>
             </ul>
           </div>
-          <div className="footer-copyright">
-            <p>Copyright information:</p>
-            <p>&copy;2023 Sugar Rush. All rights reserved</p>
-            <p>
-              Created with <span className="heart">&#10084;</span>| HYF Class41
-              Team 1
-            </p>
-          </div>
         </div>
-        <div className="footer-sugarrush">
-          <div className="footer-logo">
-            <Logo position="footer" />
-          </div>
-          <Socialmedia />
+        <div className="footer-contact">
+          <ul>
+            <li>Phone : 06-123-321-89</li>
+            <li>Email : info@sugarrush.com</li>
+            <li>Adress: Sugarrush 1 , 8937 SR Netherland</li>
+          </ul>
         </div>
+        <div className="footer-copyright">
+          <p>Copyright information:</p>
+          <p>&copy;2023 Sugar Rush. All rights reserved</p>
+          <p>
+            Created with <span className="heart">&#10084;</span>| HYF Class41
+            Team 1
+          </p>
+        </div>
+      </div>
+      <div className="footer-sugarRush">
+        <Logo position="footer" />
+        <Socialmedia />
       </div>
     </footer>
   );
