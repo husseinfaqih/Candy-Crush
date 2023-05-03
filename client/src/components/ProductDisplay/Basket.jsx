c996f7d78493473b182c079962a4be772db0a17b
mohamed belal <mhbbelal22@gmail.com>
2023-05-03 14:36:26 +0200

    creating context and adding to cart

--- client/src/components/ProductDisplay/Basket.jsx
+++ client/src/components/ProductDisplay/Basket.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { BsCart } from "react-icons/bs";
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { CartContext } from "../../store/Context";
 
-const Basket = () => {
+const Basket = ({ product }) => {
+  const cartCtx = useContext(CartContext);
+
+  const submitHandler = () => {
+    cartCtx.addItem({ ...product, amount: 1 });
+  };
   return (
-    <div>
+    <div onClick={submitHandler} style={{ cursor: "pointer" }}>
+      <span style={{ fontSize: "20px" }}>add to cart</span>
       <BsCart size={40} />
     </div>
   );
 };
 
 export default Basket;
+Basket.propTypes = {
+  product: PropTypes.object,
+};
