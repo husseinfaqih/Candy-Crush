c58ff027c2a7a7b22a30e325225979d2f50fe88b
Hussein <husseinfaqih@gmail.com>
2023-05-05 15:39:27 +0200

    added a number for added items next to basket

--- client/src/components/header/CartButton.jsx
+++ client/src/components/header/CartButton.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { CartContext } from "../../store/Context";
+import { useContext } from "react";
 import cart from "../../assets/cart.svg";
 import { Link } from "react-router-dom";
 
 function CartButton() {
+  const { items } = useContext(CartContext);
+  const sum = items
+    .map((item) => item.amount)
+    .reduce((acc, curr) => acc + curr, 0);
+
   return (
     <Link className="header-cart-button" to="/cart">
       <img className="header-cart-button-image" src={cart} alt="cart" />
+      <h3>{sum}</h3>
     </Link>
   );
 }
