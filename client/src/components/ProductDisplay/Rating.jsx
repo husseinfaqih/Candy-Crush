a3b6a8fa2ac081bef1f2f3c6afdb3c88c48131e8
Hussein <husseinfaqih@gmail.com>
2023-05-08 13:50:33 +0200

    setting up the rating system, need to send it to the backend

--- client/src/components/ProductDisplay/Rating.jsx
+++ client/src/components/ProductDisplay/Rating.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
-const Rating = ({ productRating }) => {
+const Rating = ({ productRating, product }) => {
   const filledStar = <AiFillStar size={40} />;
   const emptyStar = <AiOutlineStar size={40} />;
 
@@ -10,10 +10,22 @@ const Rating = ({ productRating }) => {
     i < productRating ? filledStar : emptyStar
   );
 
+  const updateRating = (index) => {
+    const newRating = Math.round((product.rate + index) / 2);
+    // eslint-disable-next-line no-console
+    console.log("Old Rating:", product.rate);
+    // eslint-disable-next-line no-console
+    console.log("Clicked star index:", index);
+    // eslint-disable-next-line no-console
+    console.log("new Rating:", newRating);
+  };
+
   return (
     <div>
       {stars.map((star, index) => (
-        <span key={index}>{star}</span>
+        <span key={index} onClick={() => updateRating(index + 1)}>
+          {star}
+        </span>
       ))}
     </div>
   );
@@ -21,6 +33,7 @@ const Rating = ({ productRating }) => {
 
 Rating.propTypes = {
   productRating: PropTypes.number.isRequired,
+  product: PropTypes.object.isRequired,
 };
 
 export default Rating;
