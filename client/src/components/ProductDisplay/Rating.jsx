3a5d8fc1ce776967918018dda2800c5f793e9746
Hussein <husseinfaqih@gmail.com>
2023-05-09 15:04:10 +0200

    rating is functional with user experience

--- client/src/components/ProductDisplay/Rating.jsx
+++ client/src/components/ProductDisplay/Rating.jsx
@@ -3,24 +3,29 @@ import useFetch from "../../hooks/useFetch";
 import { useState } from "react";
 import PropTypes from "prop-types";
 import { AiOutlineStar, AiFillStar } from "react-icons/ai";
+import "./Rating.css";
 
 const Rating = ({ productRating, product }) => {
+  const [rating, setRating] = useState(product.rate);
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+  const [selectedRating, setSelectedRating] = useState(0);
+
   const filledStar = <AiFillStar size={40} />;
   const emptyStar = <AiOutlineStar size={40} />;
+  const hoverStar = <AiFillStar size={40} className="hover-star" />;
 
   const stars = Array.from({ length: 5 }, (_, i) =>
     i < productRating ? filledStar : emptyStar
   );
-
-  const [rating, setRating] = useState(product.rate);
 
   const { performFetch } = useFetch(
     `/product/rate/${product._id}?rating=${rating}`
   );
 
   const updateRating = (index) => {
-    const newRating = Math.round((product.rate + index) / 2);
+    const newRating = (product.rate + index) / 2;
     setRating(newRating);
+    setSelectedRating(index);
 
     performFetch({
       method: "PUT",
@@ -30,13 +35,35 @@ const Rating = ({ productRating, product }) => {
     });
   };
 
+  const handleStarHover = (index) => {
+    setHoveredIndex(index);
+  };
+
+  const handleStarLeave = () => {
+    setHoveredIndex(-1);
+  };
+
   return (
     <div>
       {stars.map((star, index) => (
-        <span key={index} onClick={() => updateRating(index + 1)}>
-          {star}
+        <span
+          key={index}
+          onClick={() => updateRating(index + 1)}
+          onMouseEnter={() => handleStarHover(index)}
+          onMouseLeave={() => handleStarLeave()}
+        >
+          {index <= (hoveredIndex >= 0 ? hoveredIndex : selectedRating - 1)
+            ? hoverStar
+            : star}
         </span>
       ))}
+      {selectedRating > 0 && (
+        <p>
+          Thank you for your rating! You&apos;ve selected {selectedRating}{" "}
+          star(s) for this product. Your feedback is greatly appreciated and
+          will help us improve our services.
+        </p>
+      )}
     </div>
   );
 };
