c6ac98074528cfb1090ddd850f0cca41de23e65c
Hussein <husseinfaqih@gmail.com>
2023-05-09 13:24:36 +0200

    added and tested update rating method to server - needs to connect it to client

--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -4,6 +4,7 @@ import {
   createProduct,
   getOneProduct,
   getProductsByCategory,
+  updateRating,
 } from "../controllers/product.js";
 
 const productRouter = express.Router();
@@ -12,5 +13,7 @@ productRouter.get("/", getProducts); //get all products
 productRouter.post("/", createProduct); //create product
 productRouter.get("/find/:id", getOneProduct); //get one product
 productRouter.get("/:category", getProductsByCategory); //get all products one category
+
+productRouter.put("/rate/:id", updateRating); //get one product
 
 export default productRouter;
