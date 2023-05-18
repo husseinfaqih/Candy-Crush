914dc4b4e24294ca76f05a777cfcef5df7ac8d0c
Hussein <husseinfaqih@gmail.com>
2023-05-18 12:10:04 +0200

    styling done, now responsiveness

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -7,36 +7,37 @@ function Footer() {
     <footer>
       <div className="footer-information">
         <h4>Categories</h4>
-        <div className="footer-category">
-          <div className="footer-col">
-            <ul>
-              <li>Cookies</li>
-              <li>Candy Corn</li>
-              <li>Lollipop</li>
-              <li>Hard Candy</li>
-              <li>Vegetarian</li>
-            </ul>
-          </div>
+
+        <div>
+          <ul className="footer-col">
+            <li>Cookies</li>
+            <li>Candy Corn</li>
+            <li>Lollipop</li>
+            <li>Hard Candy</li>
+            <li>Vegetarian</li>
+          </ul>
         </div>
-        <div className="footer-contact">
-          <ul>
+
+        <div>
+          <ul className="footer-contact">
             <li>Phone : 06-123-321-89</li>
             <li>Email : info@sugarrush.com</li>
             <li>Adress: Sugarrush 1 , 8937 SR Netherland</li>
           </ul>
         </div>
         <div className="footer-copyright">
-          <p>Copyright information:</p>
-          <p>&copy;2023 Sugar Rush. All rights reserved</p>
-          <p>
+          <li>Copyright information:</li>
+          <li>&copy;2023 Sugar Rush. All rights reserved</li>
+          <li>
+            {" "}
             Created with <span className="heart">&#10084;</span>| HYF Class41
             Team 1
-          </p>
+          </li>
         </div>
       </div>
       <div className="footer-sugarRush">
-        <Logo position="footer" />
-        <Socialmedia />
+        <Logo className="footer-Logo" />
+        <Socialmedia className="footer-socialmedia" />
       </div>
     </footer>
   );
--- client/src/components/header/DonutOne.jsx
+++ client/src/components/header/DonutOne.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import donutOne from "../../assets/donutone.svg";
 
 function DonutOne() {
-  return <img className="header-donut donut-one" src={donutOne} alt="donut" />;
+  return <img className="donut-one" src={donutOne} alt="donut" />;
 }
 
 export default DonutOne;
